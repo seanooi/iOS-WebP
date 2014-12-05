3e3e7d73ecdfbae9a62ed00c841b1e3982ea7c0b
Kasper Weibel Nielsen-Refs <kasper@vivino.com>
2014-12-05 12:04:27 +0100

    Updated formatting

--- iOS-WebP/UIImage+WebP.h
+++ iOS-WebP/UIImage+WebP.h
@@ -12,26 +12,31 @@
 
 @interface UIImage (WebP)
 
-+ (UIImage *)imageWithWebPData:(NSData *)imgData;
-+ (UIImage *)imageWithWebP:(NSString *)filePath;
++ (UIImage*)imageWithWebPData:(NSData*)imgData;
 
-+ (NSData *)imageToWebP:(UIImage *)image quality:(CGFloat)quality;
-+ (void)imageToWebP:(UIImage *)image quality:(CGFloat)quality alpha:(CGFloat)alpha preset:(WebPPreset)preset
-    completionBlock:(void (^)(NSData *result))completionBlock
-       failureBlock:(void (^)(NSError *error))failureBlock;
-       
-+ (void)imageToWebP:(UIImage *)image
++ (UIImage*)imageWithWebP:(NSString*)filePath;
+
++ (NSData*)imageToWebP:(UIImage*)image quality:(CGFloat)quality;
+
++ (void)imageToWebP:(UIImage*)image
             quality:(CGFloat)quality
               alpha:(CGFloat)alpha
              preset:(WebPPreset)preset
-        configBlock:(void (^)(WebPConfig *config))configBlock
-    completionBlock:(void (^)(NSData *result))completionBlock
-       failureBlock:(void (^)(NSError *error))failureBlock;
+    completionBlock:(void (^)(NSData* result))completionBlock
+       failureBlock:(void (^)(NSError* error))failureBlock;
 
-+ (void)imageWithWebP:(NSString *)filePath
-      completionBlock:(void (^)(UIImage *result))completionBlock
-         failureBlock:(void (^)(NSError *error))failureBlock;
++ (void)imageToWebP:(UIImage*)image
+            quality:(CGFloat)quality
+              alpha:(CGFloat)alpha
+             preset:(WebPPreset)preset
+        configBlock:(void (^)(WebPConfig* config))configBlock
+    completionBlock:(void (^)(NSData* result))completionBlock
+       failureBlock:(void (^)(NSError* error))failureBlock;
 
-- (UIImage *)imageByApplyingAlpha:(CGFloat)alpha;
++ (void)imageWithWebP:(NSString*)filePath
+      completionBlock:(void (^)(UIImage* result))completionBlock
+         failureBlock:(void (^)(NSError* error))failureBlock;
+
+- (UIImage*)imageByApplyingAlpha:(CGFloat)alpha;
 
 @end
